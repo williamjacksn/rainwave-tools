198b05b26b1b1d49fa032688bf13b4b82034d2eb
William Jackson <william@subtlecoolness.com>
2014-11-21 09:30:13 -0600

    Implement cleanocr

--- cleanocr.py
+++ cleanocr.py
@@ -1,4 +1,50 @@
 #!/usr/bin/env python3
 
+import mutagen.id3
 import ocremix
+import os
+import re
 
+log = print
+
+cwd = os.getcwd()
+log('{} : current directory'.format(cwd))
+
+mp3s = []
+
+for dirpath, dirnames, filenames in os.walk(cwd):
+    for filename in filenames:
+        if filename.endswith('.mp3'):
+            mp3s.append(os.path.join(dirpath, filename))
+
+log('** found {} MP3s'.format(len(mp3s)))
+
+for mp3 in mp3s:
+    ocr_id = None
+    changed = False
+    tags = mutagen.id3.ID3(mp3)
+    tag_www = tags.getall('WXXX')[0].url
+    for match in re.findall('\d{5}', tag_www):
+        ocr_id = int(match)
+    if ocr_id is None:
+        log('{} : not an OCR url'.format(mp3))
+        continue
+    remix = ocremix.OCReMix(ocr_id)
+
+    if remix.info_url != tag_www:
+        log('{} : updating www to {}'.format(mp3, remix.info_url))
+        tags.delall('WXXX')
+        tags.add(mutagen.id3.WXXX(url=remix.info_url))
+        changed = True
+
+    tag_title = tags.getall('TIT2')[0][0]
+    if remix.title != tag_title:
+        log('{} : updating title to {}'.format(mp3, remix.title))
+        tags.delall('TIT2')
+        tags.add(mutagen.id3.TIT2(encoding=3, text=[remix.title]))
+        changed = True
+
+    if changed:
+        tags.save(mp3)
+    else:
+        log('{} : no change'.format(mp3))
