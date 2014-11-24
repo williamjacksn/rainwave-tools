df2815a527c2814472042058bb779e4ecc1060da
William Jackson <william@subtlecoolness.com>
2014-11-24 14:38:46 -0600

    Remove dependence on rwtag

--- getocr.py
+++ getocr.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
+import mutagen.id3
 import ocremix
 import os
 import stat
-import subprocess
 import sys
 import urllib.request
 
@@ -11,11 +11,8 @@ def log(message):
     print(message)
 
 DEST_DIR = '/home/icecast/ocr-staging'
-
-def do_cmd(cmd):
-    output = subprocess.check_output(cmd, universal_newlines=True)
-    for line in output.splitlines():
-        log(line)
+COMMENT = 'Remix Info @ OCR'
+GENRE_PROMPT = 'Enter a genre > '
 
 if len(sys.argv) > 1:
     cmd = sys.argv[1]
@@ -37,18 +34,41 @@ log('Downloading mp3 file from {}'.format(remix.mp3_url))
 temp_file, _ = urllib.request.urlretrieve(remix.mp3_url)
 log('Downloaded mp3 file to {}'.format(temp_file))
 
-do_cmd(['rwtag', 'set', 'album', remix.album, temp_file])
-do_cmd(['rwtag', 'set', 'title', remix.title, temp_file])
-do_cmd(['rwtag', 'set', 'artist', remix.artist, temp_file])
-do_cmd(['rwtag', 'set', 'www', remix.info_url, temp_file])
-do_cmd(['rwtag', 'set', 'comment', 'Remix Info @ OCR', temp_file])
-do_cmd(['rwtag', 'drop', 'genre', temp_file])
-do_cmd(['rwtag', 'drop', 'track', temp_file])
-do_cmd(['rwtag', 'drop', 'year', temp_file])
+tags = mutagen.id3.ID3(temp_file)
 
-genre = input('genre > ')
+log('Setting album to {}'.format(repr(remix.album)))
+tags.delall('TALB')
+tags.add(mutagen.id3.TALB(encoding=3, text=[remix.album]))
+
+log('Setting title to {}'.format(repr(remix.title)))
+tags.delall('TIT2')
+tags.add(mutage.id3.TIT2(encoding=3, text=[remix.title]))
+
+log('Setting artist to {}'.format(repr(remix.artist)))
+tags.delall('TPE1')
+tags.add(mutagen.id3.TPE1(encoding=3, text=[remix.artist]))
+
+log('Setting www to {}'.format(repr(remix.info_url)))
+tags.delall('WXXX')
+tags.add(mutagen.id3.WXXX(encoding=0, url=remix.info_url))
+
+log('Setting comment to {}'.format(repr(COMMENT)))
+tags.delall('COMM')
+tags.add(mutagen.id3.COMM(encoding=3, text=[COMMENT]))
+
+log('Dropping track tag')
+tags.delall('TRCK')
+
+log('Dropping year tag')
+tags.delall('TDRC')
+
+tags.delall('TCON')
+genre = input(GENRE_PROMPT)
 if genre:
-    do_cmd(['rwtag', 'set', 'genre', genre, temp_file])
+    log('Setting genre to {}'.format(repr(genre)))
+    tags.add(mutagen.id3.TCON(encoding=3, text=[genre]))
+
+tags.save(temp_file)
 
 target_file = '{}.mp3'.format(remix.safe_title)
 mp3_dest = os.path.join(DEST_DIR, remix.safe_album, target_file)
