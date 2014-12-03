5d0a31a750f1748f74bcd287ad1d996b33e3fbd2
William Jackson <william@subtlecoolness.com>
2014-12-03 14:59:37 -0600

    Add genre management tools

---
+++ apply_genres.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import mutagen.id3
+import os
+
+GENRE_FILE = '_genre.txt'
+
+
+def log(message):
+    print(message)
+
+cwd = os.getcwd()
+log('{} : current directory'.format(cwd))
+
+for root, folders, files in os.walk(cwd):
+    for filename in files:
+        if filename.endswith('.mp3'):
+            break
+    else:
+        log('{} : no mp3s'.format(root))
+        continue
+
+    this_genre_file = os.path.join(root, GENRE_FILE)
+    if GENRE_FILE in files:
+        with open(this_genre_file) as f:
+            genre = f.readline().strip()
+            log('{} : {}'.format(this_genre_file, genre))
+    else:
+        log('{} : not found'.format(this_genre_file))
+        continue
+
+    if genre == '':
+        log('{} : empty, skipping this folder'.format(this_genre_file))
+        continue
+
+    for filename in files:
+        if filename.endswith('.mp3'):
+            mp3_path = os.path.join(root, filename)
+            tags = mutagen.id3.ID3(mp3_path)
+            for tag_genre in tags.getall('TCON')[0].text:
+                current_genre = tag_genre
+                break
+            else:
+                current_genre = ''
+            if current_genre == genre:
+                log('{} already has genre {}'.format(mp3_path, current_genre))
+                continue
+            else:
+                tags.delall('TCON')
+                tags.add(mutagen.id3.TCON(encoding=3, text=[genre]))
+                tags.save()
+                log('{} : setting genre to {}'.format(mp3_path, genre))
---
+++ assign_genres.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import mutagen.id3
+import os
+
+GENRE_FILE = '_genre.txt'
+
+
+def log(message):
+    print(message)
+
+cwd = os.getcwd()
+log('{} : current directory'.format(cwd))
+
+for root, folders, files in os.walk(cwd):
+    used_genre = ''
+    has_mp3s = False
+    for filename in files:
+        if filename.endswith('.mp3'):
+            has_mp3s = True
+            if not used_genre:
+                mp3_path = os.path.join(root, filename)
+                tags = mutagen.id3.ID3(filename)
+                tag_genres = tags.getall('TCON')[0].text
+                for tag_genre in tag_genres:
+                    used_genre = tag_genre
+                    break
+    if not has_mp3s:
+        log('{} : no mp3s'.format(root))
+        continue
+
+    this_genre_file = os.path.join(root, GENRE_FILE)
+    if GENRE_FILE in files:
+        with open(this_genre_file) as f:
+            genre = f.readline().strip()
+            log('{} : {}'.format(this_genre_file, genre))
+        continue
+    try:
+        new_genre = input('{} : set genre [{}] > '.format(root, used_genre))
+        if new_genre == '':
+            continue
+        with open(this_genre_file, 'w') as f:
+            f.write('{}\n'.format(new_genre))
+    except KeyboardInterrupt:
+        print('')
+        break
