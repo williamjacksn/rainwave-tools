d2c7d768ec2712fd03d8292aeaf4cc4ca0cd80eb
William Jackson <william@subtlecoolness.com>
2014-12-03 08:37:20 -0600

    Add logging to ocra2ocr

--- ocra2ocr.py
+++ ocra2ocr.py
@@ -3,12 +3,18 @@
 import mutagen.id3
 import sys
 
+
+def log(message):
+    print(message)
+
 fn = sys.argv[1]
 url = sys.argv[2]
 
 tags = mutagen.id3.ID3(fn)
+log('{} : setting www to {}'.format(fn, url))
 tags.delall('WXXX')
 tags.add(mutagen.id3.WXXX(encoding=0, url=url))
+log('{} : setting comment to {}'.format(fn, url))
 tags.delall('COMM')
 tags.add(mutagen.id3.COMM(encoding=3, text=['Remix Info @ OCR']))
 tags.save(fn)
