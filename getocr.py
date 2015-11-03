0b50b4de97c3a725016afadcc3231e51aebb3c6b
William Jackson <william@subtlecoolness.com>
2015-11-03 14:59:41 -0600

    Wrap getocr in main()

--- getocr.py
+++ getocr.py
@@ -1,86 +1,92 @@
 #!/usr/bin/env python3
 
 import mutagen.id3
-import ocremix
 import os
+import rainwave_tools.ocremix
 import stat
 import sys
 import urllib.request
 
 
-def log(message):
-    print(message)
+def log(m):
+    print(m)
 
 DEST_DIR = '/home/icecast/ocr-staging'
 COMMENT = 'Remix Info @ OCR'
 GENRE_PROMPT = 'Enter a genre > '
 
-if len(sys.argv) > 1:
-    cmd = sys.argv[1]
-else:
-    sys.exit('Please provide an OCR ID as the first argument.')
 
-if cmd.isdigit():
-    ocr_num = int(cmd)
-else:
-    sys.exit('{} is not an integer.'.format(cmd))
+def main():
+    if len(sys.argv) > 1:
+        cmd = sys.argv[1]
+    else:
+        sys.exit('Please provide an OCR ID as the first argument.')
 
-if ocr_num < 1 or ocr_num > 99999:
-    sys.exit('{} is not between 1 and 99999'.format(ocr_num))
+    if cmd.isdigit():
+        ocr_num = int(cmd)
+    else:
+        sys.exit('{} is not an integer.'.format(cmd))
 
-log('Processing OCR{:05}'.format(ocr_num))
-remix = ocremix.OCReMix(ocr_num)
+    if ocr_num < 1 or ocr_num > 99999:
+        sys.exit('{} is not between 1 and 99999'.format(ocr_num))
 
-log('Downloading mp3 file from {}'.format(remix.mp3_url))
-temp_file, _ = urllib.request.urlretrieve(remix.mp3_url)
-log('Downloaded mp3 file to {}'.format(temp_file))
+    log('Processing OCR{:05}'.format(ocr_num))
+    remix = rainwave_tools.ocremix.OCReMix(ocr_num)
 
-tags = mutagen.id3.ID3(temp_file)
+    log('Downloading mp3 file from {}'.format(remix.mp3_url))
+    temp_file, _ = urllib.request.urlretrieve(remix.mp3_url)
+    log('Downloaded mp3 file to {}'.format(temp_file))
 
-log('Setting TALB (album) to {!r}'.format(remix.album))
-tags.delall('TALB')
-tags.add(mutagen.id3.TALB(encoding=3, text=[remix.album]))
+    tags = mutagen.id3.ID3(temp_file)
 
-log('Setting TIT2 (title) to {!r}'.format(remix.title))
-tags.delall('TIT2')
-tags.add(mutagen.id3.TIT2(encoding=3, text=[remix.title]))
+    log('Setting TALB (album) to {!r}'.format(remix.album))
+    tags.delall('TALB')
+    tags.add(mutagen.id3.TALB(encoding=3, text=[remix.album]))
 
-log('Setting TPE1 (artist) to {!r}'.format(remix.artist))
-tags.delall('TPE1')
-tags.add(mutagen.id3.TPE1(encoding=3, text=[remix.artist]))
+    log('Setting TIT2 (title) to {!r}'.format(remix.title))
+    tags.delall('TIT2')
+    tags.add(mutagen.id3.TIT2(encoding=3, text=[remix.title]))
 
-log('Setting WXXX (www) to {!r}'.format(remix.info_url))
-tags.delall('WXXX')
-tags.add(mutagen.id3.WXXX(encoding=0, url=remix.info_url))
+    log('Setting TPE1 (artist) to {!r}'.format(remix.artist))
+    tags.delall('TPE1')
+    tags.add(mutagen.id3.TPE1(encoding=3, text=[remix.artist]))
 
-log('Setting COMM (comment) to {!r}'.format(COMMENT))
-tags.delall('COMM')
-tags.add(mutagen.id3.COMM(encoding=3, text=[COMMENT]))
+    log('Setting WXXX (www) to {!r}'.format(remix.info_url))
+    tags.delall('WXXX')
+    tags.add(mutagen.id3.WXXX(encoding=0, url=remix.info_url))
 
-if remix.has_lyrics:
-    log('** This remix has lyrics!')
+    log('Setting COMM (comment) to {!r}'.format(COMMENT))
+    tags.delall('COMM')
+    tags.add(mutagen.id3.COMM(encoding=3, text=[COMMENT]))
 
-tags.delall('TCON')
-genre = input(GENRE_PROMPT)
-if genre:
-    log('Setting TCON (genre) to {!r}'.format(genre))
-    tags.add(mutagen.id3.TCON(encoding=3, text=[genre]))
+    if remix.has_lyrics:
+        log('** This remix has lyrics!')
 
-log('Dropping unnecessary tags')
-for tag in ['APIC', 'TCMP', 'TCOM', 'TCOP', 'TDRC', 'TENC', 'TIT1', 'TIT3',
-            'TOAL', 'TOPE', 'TPE2', 'TPUB', 'TRCK', 'TSSE', 'TXXX', 'USLT',
-            'WOAR']:
-    tags.delall(tag)
+    tags.delall('TCON')
+    genre = input(GENRE_PROMPT)
+    if genre:
+        log('Setting TCON (genre) to {!r}'.format(genre))
+        tags.add(mutagen.id3.TCON(encoding=3, text=[genre]))
 
-tags.save(temp_file)
+    log('Dropping unnecessary tags')
+    for tag in ['APIC', 'TCMP', 'TCOM', 'TCOP', 'TDRC', 'TENC', 'TIT1', 'TIT3',
+                'TOAL', 'TOPE', 'TPE2', 'TPUB', 'TRCK', 'TSSE', 'TXXX', 'USLT',
+                'WOAR']:
+        tags.delall(tag)
 
-target_file = '{}.mp3'.format(remix.safe_title)
-mp3_dest = os.path.join(DEST_DIR, remix.safe_album, target_file)
-log('Moving {} to {}'.format(temp_file, mp3_dest))
-os.renames(temp_file, mp3_dest)
+    tags.save(temp_file)
 
-log('Changing file permissions')
-os.chmod(mp3_dest, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP)
+    target_file = '{}.mp3'.format(remix.safe_title)
+    mp3_dest = os.path.join(DEST_DIR, remix.safe_album, target_file)
+    log('Moving {} to {}'.format(temp_file, mp3_dest))
+    os.renames(temp_file, mp3_dest)
 
-log('Cleaning up temporary files')
-urllib.request.urlcleanup()
+    log('Changing file permissions')
+    perms = stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP
+    os.chmod(mp3_dest, perms)
+
+    log('Cleaning up temporary files')
+    urllib.request.urlcleanup()
+
+if __name__ == '__main__':
+    main()
