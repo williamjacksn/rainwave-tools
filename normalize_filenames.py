1b9975a9f18086e65d4da46c5c9cd7dae4e5a984
William Jackson <william@subtlecoolness.com>
2015-11-03 14:49:09 -0600

    Clean up `normalize_filenames`

--- normalize_filenames.py
+++ normalize_filenames.py
@@ -2,39 +2,43 @@
 
 import argparse
 import mutagen.id3
-import ocremix
 import pathlib
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-a', '--include-album', action='store_true')
-parser.add_argument('dir', nargs='?', default='.')
-
-args = parser.parse_args()
+import rainwave_tools.ocremix
 
 
-def log(message):
-    print('{}'.format(message))
+def log(m):
+    print('{}'.format(m))
 
-cwd = pathlib.Path(args.dir).resolve()
 
-mp3s = []
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--include-album', action='store_true')
+    parser.add_argument('dir', nargs='?', default='.')
+    args = parser.parse_args()
 
-for item in cwd.iterdir():
-    if item.is_file() and item.suffix == '.mp3':
-        mp3s.append(item)
+    cwd = pathlib.Path(args.dir).resolve()
 
-mp3s.sort()
+    mp3s = []
 
-for mp3 in mp3s:
-    log(mp3)
-    tags = mutagen.id3.ID3(str(mp3))
-    album = tags.getall('TALB')[0].text[0]
-    safe_album = ocremix.OCReMix.make_safe(album)
-    title = tags.getall('TIT2')[0].text[0]
-    safe_title = ocremix.OCReMix.make_safe(title)
-    new_name = safe_title + '.mp3'
-    if args.include_album:
-        new_name = safe_album + '_' + new_name
-    new_file = mp3.with_name(new_name)
-    log('-> {}'.format(new_file))
-    mp3.rename(new_file)
+    for item in cwd.iterdir():
+        if item.is_file() and item.suffix == '.mp3':
+            mp3s.append(item)
+
+    mp3s.sort()
+
+    for mp3 in mp3s:
+        log(mp3)
+        tags = mutagen.id3.ID3(str(mp3))
+        album = tags.getall('TALB')[0].text[0]
+        safe_album = rainwave_tools.ocremix.OCReMix.make_safe(album)
+        title = tags.getall('TIT2')[0].text[0]
+        safe_title = rainwave_tools.ocremix.OCReMix.make_safe(title)
+        new_name = safe_title + '.mp3'
+        if args.include_album:
+            new_name = safe_album + '_' + new_name
+        new_file = mp3.with_name(new_name)
+        log('-> {}'.format(new_file))
+        mp3.rename(new_file)
+
+if __name__ == '__main__':
+    main()
