849c11e858a2f5b747d2ae692f59639d997335e2
William Jackson <william@subtlecoolness.com>
2015-11-03 15:02:11 -0600

    Wrap ocra2ocr in main()

--- ocra2ocr.py
+++ ocra2ocr.py
@@ -4,18 +4,23 @@ import mutagen.id3
 import sys
 
 
-def log(message):
-    print(message)
+def log(m):
+    print(m)
 
-fn = sys.argv[1]
-url = sys.argv[2]
 
-tags = mutagen.id3.ID3(fn)
-log('{} : setting www to {}'.format(fn, url))
-tags.delall('WXXX')
-tags.add(mutagen.id3.WXXX(encoding=0, url=url))
-comment = 'Remix Info @ OCR'
-log('{} : setting comment to {!r}'.format(fn, comment))
-tags.delall('COMM')
-tags.add(mutagen.id3.COMM(encoding=3, text=[comment]))
-tags.save(fn)
+def main():
+    fn = sys.argv[1]
+    url = sys.argv[2]
+
+    tags = mutagen.id3.ID3(fn)
+    log('{} : setting www to {}'.format(fn, url))
+    tags.delall('WXXX')
+    tags.add(mutagen.id3.WXXX(encoding=0, url=url))
+    comment = 'Remix Info @ OCR'
+    log('{} : setting comment to {!r}'.format(fn, comment))
+    tags.delall('COMM')
+    tags.add(mutagen.id3.COMM(encoding=3, text=[comment]))
+    tags.save(fn)
+
+if __name__ == '__main__':
+    main()
