a3c593a54d078b5cfc89391b4eab21f9a5c2ac82
William Jackson <william@subtlecoolness.com>
2017-02-17 15:50:36 -0600

    Fix `titles` when there is no existing title

--- rainwave_tools/titles.py
+++ rainwave_tools/titles.py
@@ -1,26 +1,31 @@
 import argparse
+import logging
 import mutagen.id3
-import rainwave_tools.utils
+import rainwave_tools
+import sys
 
-
-def log(m):
-    print(m)
+log = logging.getLogger(__name__)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('path', nargs='*', default='.')
+    parser.add_argument('path', nargs='+')
     return parser.parse_args()
 
 
 def main():
+    logging.basicConfig(stream=sys.stdout, level='INFO', format='%(message)s')
     args = parse_args()
 
     change_count = 0
 
     for mp3 in rainwave_tools.utils.get_mp3s(args.path):
         tags = mutagen.id3.ID3(str(mp3))
-        title = tags.getall('TIT2')[0].text[0]
+        titles = tags.getall('TIT2')
+        if titles:
+            title = tags.getall('TIT2')[0].text[0]
+        else:
+            title = ''
         new_title = input('{} [{}] > '.format(mp3, title))
         if new_title:
             change_count += 1
@@ -31,7 +36,7 @@ def main():
     m = '** updated tags in {} file'.format(change_count)
     if change_count != 1:
         m = '{}s'.format(m)
-    log(m)
+    log.info(m)
 
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="rainwave-tools",
-    version='0.6.2',
+    version='0.6.3',
     author='William Jackson',
     author_email='william@subtlecoolness.com',
     url='https://github.com/williamjacksn/rainwave_tools',
