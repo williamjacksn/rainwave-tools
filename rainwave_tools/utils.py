f686d5b002fdf27782d5a3c9b5dbf3d09ca3c3f9
William Jackson <william@subtlecoolness.com>
2020-07-06 15:21:03 -0500

    Add some characters to remove in make_safe

--- rainwave_tools/utils.py
+++ rainwave_tools/utils.py
@@ -58,7 +58,7 @@ def get_mp3s(paths: Any):
 # }
 
 def make_safe(s):
-    translate_table = {ord(char): None for char in ' !"#%&\'()*+,-./:;<=>?@[\\]^_`{|}~–—あいごま'}
+    translate_table = {ord(char): None for char in ' !"#%&\'()*+,-./:;<=>?@[\\]^_`{|}~–—あいごま고말싶은하'}
     special = dict(zip(map(ord, '²ÉÜàáãäçèéêíðñóöüşКСавеийкмност'), '2EUaaaaceeeidnoousKSaveijkmnost'))
     translate_table.update(special)
     return s.translate(translate_table)
