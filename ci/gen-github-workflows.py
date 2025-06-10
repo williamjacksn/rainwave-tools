3e366d6286e11c54962e580cf6dede8a8cb7a566
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-06-10 14:43:01 -0500

    Gen yaml

---
+++ ci/gen-github-workflows.py
@@ -0,0 +1,26 @@
+import gen
+
+publish = {
+    "name": "Publish the release package to PyPI",
+    "on": {"release": {"types": ["published"]}},
+    "jobs": {
+        "publish": {
+            "name": "Publish the release package to PyPI",
+            "runs-on": "ubuntu-latest",
+            "environment": {
+                "name": "pypi-release",
+                "url": "https://pypi.org/p/rainwave-tools",
+            },
+            "permissions": {"id-token": "write"},
+            "steps": [
+                {"name": "Check out the repository", "uses": "actions/checkout@v4"},
+                {
+                    "name": "Build and publish the package",
+                    "run": "sh ci/build-and-publish.sh",
+                },
+            ],
+        }
+    },
+}
+
+gen.gen(publish, ".github/workflows/publish-release-to-pypi.yaml")
